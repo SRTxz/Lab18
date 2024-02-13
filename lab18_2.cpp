27abd10e97ecf85f2e85688aa0033630e13e1718
Toey <118428254+SRTxz@users.noreply.github.com>
2024-02-13 15:21:11 +0700

    completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+struct Rect
+{
+	double x, y, w, h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *r1, Rect *r2)
+{
+	double width = max(0.0, min(r1->x + r1->w, r2->x + r2->w) - max(r1->x, r2->x));
+	double height = max(0.0, min(r1->y, r2->y) - max(r1->y - r1->h, r2->y - r2->h));
+	return width * height;
 }
